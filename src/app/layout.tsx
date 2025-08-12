df7b0d93760452cf9e174066b03bbdf03510a7d8
blesswrld <roninfortnite71@gmail.com>
2025-08-12 18:35:26 +0300

    delete unnecessary imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { cn } from "@/lib/utils";
 import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = Geist({
